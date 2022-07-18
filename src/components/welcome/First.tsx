2ae0b5e6d12d057a63f56e10e4cd7f942153ac95
ferrytsui <ferrytsui@163.com>
2022-07-19 04:11:42 +0800

    重构提取欢迎页面组件

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,22 +1,22 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import welcome1 from '../../assets/icons/welcome1.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout'
 
 export const First = defineComponent({
   setup: (props, context) => {
+    const slots = {
+      icon: () => <img src={welcome1} />,
+      title: () => <h2>薯条记账<br/>你的省钱助手</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
+        <RouterLink to="/welcome/2">下一页</RouterLink> 
+        <RouterLink to="/start">跳过</RouterLink> 
+      </>
+    }
     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.welcome1} src={welcome1}/>
-          <h2>薯条记账<br/>你的省钱助手</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
-          <RouterLink to="/welcome/2">下一页</RouterLink> 
-          <RouterLink to="/start">跳过</RouterLink> 
-        </div>
-      </div>
+      <WelcomeLayout v-slots={slots} />
     )
   }
 })
--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,22 +1,23 @@
 import { defineComponent } from 'vue';
-import s from './First.module.scss'
+import s from './WelcomeLayout.module.scss'
 import welcome4 from '../../assets/icons/welcome4.svg'
 import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout'
 
 export const Forth = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div class={s.wrapper}>
-        <div class={s.card}>
-          <img class={s.welcome1} src={welcome4}/>
-          <h2>云备份<br/>数据轻松同步上云</h2>
-        </div>
-        <div class={s.actions}>
-          <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
-          <RouterLink to="/start">完成</RouterLink> 
-          <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
-        </div>
-      </div>
+      <WelcomeLayout>
+        {{
+          icon: () => <img class={s.welcome1} src={welcome4}/>,
+          title: () => <h2>云备份<br/>数据轻松同步上云</h2>,
+          buttons: () => <>
+            <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
+            <RouterLink to="/start">完成</RouterLink> 
+            <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
+          </>
+        }}
+      </WelcomeLayout>
     )
   }
 })
