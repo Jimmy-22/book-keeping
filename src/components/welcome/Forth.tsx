cf02316856764b51e406e2604ca3807e803e41cb
ferrytsui <ferrytsui@163.com>
2022-07-19 03:43:52 +0800

    欢迎页面ok

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,9 +1,22 @@
 import { defineComponent } from 'vue';
+import s from './First.module.scss'
+import welcome4 from '../../assets/icons/welcome4.svg'
+import { RouterLink } from 'vue-router';
 
 export const Forth = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>Forth</div>
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.welcome1} src={welcome4}/>
+          <h2>云备份<br/>数据轻松同步上云</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
+          <RouterLink to="/start">完成</RouterLink> 
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
+        </div>
+      </div>
     )
   }
 })
--- src/components/welcome/Third.tsx
+++ src/components/welcome/Third.tsx
@@ -1,9 +1,22 @@
 import { defineComponent } from 'vue';
+import s from './First.module.scss'
+import welcome3 from '../../assets/icons/welcome3.svg'
+import { RouterLink } from 'vue-router';
 
 export const Third = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>Third</div>
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img class={s.welcome1} src={welcome3}/>
+          <h2>数据可视化<br/>收支一目了然</h2>
+        </div>
+        <div class={s.actions}>
+          <RouterLink class={s.fake} to="/start">跳过</RouterLink> 
+          <RouterLink to="/welcome/4">下一页</RouterLink> 
+          <RouterLink to="/start">跳过</RouterLink> 
+        </div>
+      </div>
     )
   }
 })
