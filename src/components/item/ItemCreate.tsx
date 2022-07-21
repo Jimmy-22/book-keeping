d93fc468dcc7fa574f990a851382028c3aa720d4
ferrytsui <ferrytsui@163.com>
2022-07-22 05:13:59 +0800

    feat: Tabs组件的基本功能

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,7 +1,8 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './ItemCreate.module.scss'
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
+import { Tabs, Tab } from '../../shared/Tabs';
 export const ItemCreate = defineComponent({
   props: {
     name: {
@@ -9,11 +10,21 @@ export const ItemCreate = defineComponent({
     }
   },
   setup: (props, context) => {
+    const moneyKind = ref('支出')
     return () => (
       <MainLayout>{{
         title: () => '记一笔',
         icon: () => <Icon class={s.navIcon} name="cancel" />  ,
-        default: () => <div>main</div>
+        default: () => <>
+          <Tabs v-model:selected={moneyKind.value}>
+            <Tab name='支出'>
+              icon列表1
+            </Tab>
+            <Tab name='收入'>
+              icon列表2
+            </Tab>
+          </Tabs>
+        </>
       }}</MainLayout> 
     )
   }
