5dd52db13efc49d819549168ea92039ea6cc1ab1
ferrytsui <ferrytsui@163.com>
2022-07-22 02:56:18 +0800

    初步完成Navbar组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,18 +1,21 @@
 import { defineComponent, mergeProps, PropType } from "vue";
 import s from './Icon.module.scss'
 
-export type IconName = 'plus' | 'chart' | 'welcome1' | 'welcome2' | 'welcome3' | 'welcome4' | 'menu'
+export type IconName = 'plus' | 'charts' | 'welcome1' | 'welcome2' | 'welcome3' | 'welcome4' | 'menu' | 'notify' | 'export'
 
 export const Icon = defineComponent({
   props: {
     name: {
       type: String as PropType<IconName>,
       required: true
+    },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>
     }
   },
   setup: (props, context) => {
     return () => (
-      <svg class={s.icon}>
+      <svg class={s.icon} onClick={props.onClick}>
         <use xlinkHref={'#' + props.name}></use>
       </svg>
     )
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,30 +1,33 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import s from './StartPage.module.scss';
 import { Button } from '../shared/Button';
 import { Center } from '../shared/Center';
 import { Navbar } from '../shared/Navbar';
 import { PlusButton } from '../shared/PlusButton';
 import { Icon } from "../shared/Icon";
+import { Overlay } from "../shared/Overlay";
 export const StartPage = defineComponent({
   setup: (props, context) => {
-    const onClick = () => {
-      console.log('hi')
+    const overlayVisible = ref(false)
+    const onClickMenu = () => {
+      overlayVisible.value = !overlayVisible.value
     }
     return () => (
       <div>
         <Navbar>{
           {
             default: () => '薯条记账',
-            icon: () => <Icon name="menu" class={s.navIcon}/>
+            icon: () => <Icon name="menu" onClick={onClickMenu} class={s.navIcon}/>
           }
         }</Navbar>
         <Center class={s.welcome1_wrapper}>
           <Icon name="welcome1" style={{width: '128px', height: '128px'}}/>
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>开始记账</Button>
+          <Button class={s.button}>开始记账</Button>
         </div>
         <PlusButton iconName='plus' />
+        { overlayVisible.value && <Overlay onClose={() => {overlayVisible.value = false}}></Overlay>}
       </div>
     )
   }
