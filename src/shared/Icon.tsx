385da6083c740f59d2ce9c17936bf9bd3fc1be25
ferrytsui <ferrytsui@163.com>
2022-07-21 19:52:58 +0800

    封装Icon公共组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,17 @@
+import { defineComponent, mergeProps, PropType } from "vue";
+import s from './Icon.module.scss'
+
+export const Icon = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<'add' | 'chart' | 'welcome1' | 'welcome2' | 'welcome3' | 'welcome4'>
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <svg class={s.icon}>
+        <use xlinkHref={'#' + props.name}></use>
+      </svg>
+    )
+  }
+})
