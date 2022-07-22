63346f9c08be6e2d69f24d59ee04dccc4bc2979e
ferrytsui <ferrytsui@163.com>
2022-07-23 04:53:48 +0800

    InputPad的数字输入显示

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -12,7 +12,24 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const now = new Date()
     const refDate = ref<Date>(now)
-    const appendText = (n: number | string) => { refAmount.value += n.toString() }
+    const appendText = (n: number | string) => { 
+      const nString = n.toString()
+      const dotIndex = refAmount.value.indexOf('.')
+      if (refAmount.value.length >= 13) { return } 
+      if (dotIndex > 0 && refAmount.value.length - dotIndex > 2) { return } 
+      if (nString === '.') {
+        if (dotIndex > 0) { return } // 已经有小数点
+      } else if (nString === '0') {
+        if (dotIndex === -1) {  // 没有小数点
+          if (refAmount.value === '0') { return } 
+        }
+      } else {
+        if (refAmount.value === '0') {
+          refAmount.value = ''
+        }
+      }
+      refAmount.value += n.toString()
+    }
     const buttons = [
       { text: '1', onClick: () => { appendText(1) } },
       { text: '2', onClick: () => { appendText(2) } },
@@ -25,14 +42,14 @@ export const InputPad = defineComponent({
       { text: '9', onClick: () => { appendText(9) } },
       { text: '.', onClick: () => { appendText('.') } },
       { text: '0', onClick: () => { appendText(0) } },
-      { text: '清空', onClick: () => { } },
+      { text: '清空', onClick: () => { refAmount.value = '0' } },
       { text: '提交', onClick: () => { } },
     ]
     const datePickerVisible = ref(false)
     const showDatePicker = () => datePickerVisible.value = true
     const hideDatePicker = () => datePickerVisible.value = false
     const setDate = (date: Date) => { refDate.value = date; hideDatePicker()}
-    const refAmount = ref('')
+    const refAmount = ref('0')
     return () => <>
       <div class={s.dateAndAmount} >
         <span class={s.date}>
