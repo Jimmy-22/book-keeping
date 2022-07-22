d4c5683fbe0d552cc914f10e67dc2bce6fcbff82
ferrytsui <ferrytsui@163.com>
2022-07-23 04:25:38 +0800

    数据键盘布局改造

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -12,28 +12,27 @@ export const InputPad = defineComponent({
   setup: (props, context) => {
     const now = new Date()
     const refDate = ref<Date>(now)
+    const appendText = (n: number | string) => { refAmount.value += n.toString() }
     const buttons = [
-      { text: '1', onclick: () => {} },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
+      { text: '1', onClick: () => { appendText(1) } },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
       { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '删', onClick: () => { } },
       { text: '提交', onClick: () => { } },
     ]
     const datePickerVisible = ref(false)
     const showDatePicker = () => datePickerVisible.value = true
     const hideDatePicker = () => datePickerVisible.value = false
     const setDate = (date: Date) => { refDate.value = date; hideDatePicker()}
+    const refAmount = ref('')
     return () => <>
       <div class={s.dateAndAmount} >
         <span class={s.date}>
@@ -45,7 +44,7 @@ export const InputPad = defineComponent({
             </Popup>
           </span>
         </span>
-        <span class={s.amount}>999</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(button => 
