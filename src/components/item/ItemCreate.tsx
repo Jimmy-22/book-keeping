3958ad51b99f87a8d3b0cb617e76d0d649131290
ferrytsui <ferrytsui@163.com>
2022-07-24 20:00:19 +0800

    feat：记账页面的样式

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -12,21 +12,71 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const moneyKind = ref('支出')
+    const expensesTags = ref([
+      { id: 1, name: '餐饮', sign: '￥', category: 'expenses' },
+      { id: 2, name: '购物', sign: '￥', category: 'expenses' },
+      { id: 3, name: '日用', sign: '￥', category: 'expenses' },
+      { id: 4, name: '交通', sign: '￥', category: 'expenses' },
+      { id: 5, name: '蔬菜', sign: '￥', category: 'expenses' },
+      { id: 6, name: '水果', sign: '￥', category: 'expenses' },
+      { id: 7, name: '运动', sign: '￥', category: 'expenses' },
+    ])
+    const incomeTags = ref([
+      { id: 1, name: '工资', sign: '￥', category: 'income' },
+      { id: 2, name: '兼职', sign: '￥', category: 'income' },
+      { id: 3, name: '理财', sign: '￥', category: 'income' },
+      { id: 4, name: '礼金', sign: '￥', category: 'income' },
+      { id: 5, name: '其他', sign: '￥', category: 'income' },
+    ])
     return () => (
-      <MainLayout>{{
-        title: () => '记一笔',
+      <MainLayout class={s.layout}>{{
+        title: () => '记账',
         icon: () => <Icon class={s.navIcon} name="cancel" />  ,
         default: () => <>
-          <Tabs v-model:selected={moneyKind.value}>
-            <Tab name='支出'>
-              icon列表1
-            </Tab>
-            <Tab name='收入'>
-              icon列表2
-            </Tab>
-          </Tabs>
-          <div class={s.inputPad_wrapper}>
-            <InputPad />
+          <div class={s.wrapper}>
+            <Tabs v-model:selected={moneyKind.value} class={s.tabs}>
+              <Tab name='支出' class={s.tags_wrapper}>
+                <div class={s.tag}>
+                  <div class={s.sign}>
+                    <Icon name='plus' class={s.createTag} />
+                  </div>
+                  <div class={s.name}>新增</div>
+                </div>
+                {expensesTags.value.map(tag => 
+                  <div class={[s.tag, s.selected]}>
+                    <div class={s.sign}>
+                      {tag.sign}
+                    </div>
+                    <div class={s.name}>
+                      {tag.name}
+                    </div>
+                  </div>  
+                )}
+              </Tab>
+              <Tab name="收入" class={s.tags_wrapper}>
+                <div class={s.tag}>
+                  <div class={s.sign}>
+                    <Icon name="plus" class={s.createTag} />
+                  </div>
+                  <div class={s.name}>
+                    新增
+                  </div>
+                </div>
+                {incomeTags.value.map(tag =>
+                  <div class={[s.tag, s.selected]}>
+                    <div class={s.sign}>
+                      {tag.sign}
+                    </div>
+                    <div class={s.name}>
+                      {tag.name}
+                    </div>
+                  </div>
+                )}
+              </Tab>
+            </Tabs>
+            <div class={s.inputPad_wrapper}>
+              <InputPad />
+            </div>
           </div>
         </>
       }}</MainLayout> 
